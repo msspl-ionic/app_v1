c2f9b6bf45fdf906988cffed1e4f03992ba1a972
MSSPL-JayantaRajbanshi <jayanta.rajbanshi@massoftind.com>
2022-06-21 18:34:49 +0530

    work on dashboard

--- src/app/pages/dashboard/dashboard.page.ts
+++ src/app/pages/dashboard/dashboard.page.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { ApiService } from '../../shared/services/api.service';
+interface User {
+  id: number;
+  first: string;
+  last: string;
+}
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.page.html',
@@ -9,9 +14,32 @@ import { ApiService } from '../../shared/services/api.service';
 export class DashboardPage implements OnInit {
   private subscriptions: Subscription[] = [];
   constructor(private service: ApiService,) { }
+  public categoryListArr:  any = [];
+  public featuredProductList: any = [];
+  public priceVal : any = {};
+  public sellPrice : any = {};
+  users: User[] = [
+    {
+      id: 1,
+      first: 'Alice',
+      last: 'Smith',
+    },
+    {
+      id: 2,
+      first: 'Bob',
+      last: 'Davis',
+    },
+    {
+      id: 3,
+      first: 'Charlie',
+      last: 'Rosenburg',
+    }
+  ];
+
 
   ngOnInit() {
     this.categoryList();
+    this.featuredProduct();
   }
   // slider jayanta
   slideOptsOne = {
@@ -35,16 +63,45 @@ export class DashboardPage implements OnInit {
       parent_cat_id :0,
       lang_name:2,
       device_os:"and"
-    };
-    
-		console.log(param);
+    };    
     // return;
 		this.subscriptions.push(this.service.ApiCall(param, `category/list`, 'POST').subscribe(result => {
-      console.log(result);
+      this.categoryListArr = result.response.data.category_list;
 
 		}, apiError => {
         console.log('API error');
 		}))
 	}
+
+  featuredProduct() {
+		let param: any = {
+      lang_name:2,
+      device_os:"and",
+      list_type:1
+    };    
+    // return;
+		this.subscriptions.push(this.service.ApiCall(param, `product/bestfeaturedproduct`, 'POST').subscribe(result => {
+      this.featuredProductList = result.response.data.featured_product;
+      console.log(result,"result")
+
+		}, apiError => {
+        console.log('API error');
+		}))
+	}
+  valCheck(evt,id) {
+    console.log(evt,id)
+    console.log(evt.sell_price);
+    this.sellPrice = evt.sell_price
+    // this.priceVal = evt;
+    // console.log(evt,this.priceVal);
+    
+    
+  }
+
+
+  // compareWith(o1: User, o2: User) {
+  //   return o1 && o2 ? o1.id === o2.id : o1 === o2;
+  // }
+  
 
 }
