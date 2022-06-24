286145e29e01b3f707d8653f7fe3d22a87e7fdb6
mahadev-m <mahadev.m@appnosticworx.com>
2022-06-24 13:22:06 +0530

    changes in signup page

--- src/app/auth/signup/signup.module.ts
+++ src/app/auth/signup/signup.module.ts
@@ -8,13 +8,19 @@ import { SignupPageRoutingModule } from './signup-routing.module';
 
 import { SignupPage } from './signup.page';
 
+import { AgmCoreModule } from '@agm/core';
+
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
     IonicModule,
-    SignupPageRoutingModule
+    SignupPageRoutingModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyD0wD_ve4ht6PdCyjAYSHx1E_xzZBcn9Ec', 
+      libraries: ['places']
+    })
   ],
   declarations: [SignupPage]
 })
