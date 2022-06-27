822b4925c884b78a2657fdda63795fd31b6e88ad
Suresh Das <suresh.das@massoftind.com>
2022-06-27 18:34:04 +0530

    update profile page addd

---
+++ src/app/auth/update-profile/update-profile.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { UpdateProfilePageRoutingModule } from './update-profile-routing.module';
+
+import { UpdateProfilePage } from './update-profile.page';
+
+import { HeaderModule } from 'src/app/component/header/header.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    IonicModule,
+    UpdateProfilePageRoutingModule,
+    HeaderModule
+  ],
+  declarations: [UpdateProfilePage]
+})
+export class UpdateProfilePageModule {}
